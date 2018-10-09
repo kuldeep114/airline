23bc137d0595eef549e0c5d0208828cc5b7f7b05
kuldeep114 <eleckuldeep@gmail.com>
2018-10-09 23:27:27 +0530

    adding template

--- flights/views.py
+++ flights/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight
 
 # Create your views here.
 
 
 def index(request):
-    # context = {
-    #     'flights': Flight.objects.all()
-    # }
-    return render(request, "flights/index.html")
+    context = {
+        'flights': Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
